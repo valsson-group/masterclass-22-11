77f54291d4874dd14b7f25a62d0fa70e4e611fc9
Omar Valsson <omar.valsson@gmail.com>
2022-07-04 08:24:50 -0500

    fix geo

--- Exercise-4/generate-tpr-file.sh
+++ Exercise-4/generate-tpr-file.sh
@@ -1,4 +1,4 @@
 GMX_BIN=gmx_mpi
-StartingGeometry=NaCl_StartingStructure-2.gro
+StartingGeometry=NaCl_StartingStructure-1.gro
 RunFilename=NaCl_VES_NPT-300K
 ${GMX_BIN}  grompp -f MD-NPT.mdp -c ${StartingGeometry} -p NaCl.top -o ${RunFilename}.tpr  -maxwarn 1
